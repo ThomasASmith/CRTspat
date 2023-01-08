99299e6d7029ed2cd43c6ace552814694108878a
ThomasASmith <thomas-a.smith@unibas.ch>
2023-01-08 12:08:07 +0100

    reformatted utils.R to make code tidy

--- R/utils.R
+++ R/utils.R
@@ -5,21 +5,23 @@
 #' @param auxiliaries a vector of the names of auxiliary variables to be summed across locations
 #' @return A dataframe with unique locations in which the numerators and denominators are summed over the locations
 #' @export
-Aggregate_CRT = function(trial,auxiliaries=NULL){
-  x=y=NULL
-  df= trial %>% distinct(x, y, .keep_all = TRUE)
-  df=df[order(df$x,df$y),]
-  if(!is.null(auxiliaries)){
-    for(i in 1:length(auxiliaries)){
-      varName=auxiliaries[i]
-      df1 = trial %>%
-        group_by(x,y) %>%
-        summarize(sumVar = sum(get(varName),na.rm = TRUE), .groups = 'drop')
-      df1=df1[order(df1$x,df1$y),]
-      df[[varName]] = df1$sumVar
+Aggregate_CRT <- function(trial, auxiliaries = NULL) {
+  x <- y <- NULL
+  df <- trial %>%
+    distinct(x, y, .keep_all = TRUE)
+  df <- df[order(df$x, df$y), ]
+  if (!is.null(auxiliaries)) {
+    for (i in 1:length(auxiliaries)) {
+      varName <- auxiliaries[i]
+      df1 <- trial %>%
+        group_by(x, y) %>%
+        summarize(sumVar = sum(get(varName), na.rm = TRUE), .groups = "drop")
+      df1 <- df1[order(df1$x, df1$y), ]
+      df[[varName]] <- df1$sumVar
     }
   }
-  return(df)}
+  return(df)
+}
 
 
 #' Randomize a two-armed cluster randomized trial
@@ -36,37 +38,34 @@ Aggregate_CRT = function(trial,auxiliaries=NULL){
 #' @examples
 #' #Randomize the clusters in the example trial
 #' set.seed(352)
-#' exampletrial=Randomize_CRT(testClusters)
-Randomize_CRT = function(trial,
-    matchedPair = TRUE,
-    baselineNumerator='base_num',
-    baselineDenominator='base_denom'){
+#' exampletrial <- Randomize_CRT(testClusters)
+Randomize_CRT <- function(trial, matchedPair = TRUE, baselineNumerator = "base_num", baselineDenominator = "base_denom") {
 
-  cluster= base_num= base_denom= NULL
-  trial$cluster = as.factor(trial$cluster)
+  cluster <- base_num <- base_denom <- NULL
+  trial$cluster <- as.factor(trial$cluster)
   # Randomization, assignment to arms
-  nclusters=length(unique(trial$cluster))
-  #uniformly distributed numbers, take mean and boolean of that
-  rand_numbers <- runif(nclusters,0,1)
-  if(!is.null(trial[[baselineNumerator]]) & matchedPair){
-    trial$base_num = trial[[baselineNumerator]]
-    trial$base_denom = trial[[baselineDenominator]]
-    cdf = data.frame(trial %>%
-              group_by(cluster) %>%
-              dplyr::summarize(positives = sum(base_num),
-                               total = sum(base_denom)))
-    cdf$p = cdf$positives/cdf$total
-    cdf = cdf[order(cdf$p),]
-    cdf$pair = rep(seq(1,nclusters/2),2)
-    cdf$rand_numbers = rand_numbers
-    cdf = cdf[with(cdf,order(pair,rand_numbers)),]
-    cdf$arm = rep(c(1,0),nclusters/2)
-    arm = cdf$arm[order(cdf$cluster)]
+  nclusters <- length(unique(trial$cluster))
+  # uniformly distributed numbers, take mean and boolean of that
+  rand_numbers <- runif(nclusters, 0, 1)
+  if (!is.null(trial[[baselineNumerator]]) & matchedPair) {
+    trial$base_num <- trial[[baselineNumerator]]
+    trial$base_denom <- trial[[baselineDenominator]]
+    cdf <- data.frame(trial %>%
+                        group_by(cluster) %>%
+                        dplyr::summarize(positives = sum(base_num), total = sum(base_denom)))
+    cdf$p <- cdf$positives/cdf$total
+    cdf <- cdf[order(cdf$p), ]
+    cdf$pair <- rep(seq(1, nclusters/2), 2)
+    cdf$rand_numbers <- rand_numbers
+    cdf <- cdf[with(cdf, order(pair, rand_numbers)), ]
+    cdf$arm <- rep(c(1, 0), nclusters/2)
+    arm <- cdf$arm[order(cdf$cluster)]
   } else {
-    arm <- ifelse(rand_numbers > median(rand_numbers),1,0)
+    arm <- ifelse(rand_numbers > median(rand_numbers), 1, 0)
   }
-  trial$arm <- factor(arm[trial$cluster[]], levels= c(0,1),labels = c("control", "intervention"))
-return(trial)}
+  trial$arm <- factor(arm[trial$cluster[]], levels = c(0, 1), labels = c("control", "intervention"))
+  return(trial)
+}
 
 
 #' Specification of buffer zone in a cluster randomized trial
@@ -81,50 +80,50 @@ return(trial)}
 #'
 #' @examples
 #' #Specify a buffer of 200m
-#' exampletrial=Specify_CRTbuffer(testArms, bufferWidth=0.2)
+#' exampletrial <- Specify_CRTbuffer(testArms, bufferWidth = 0.2)
 #'
-Specify_CRTbuffer= function(trial=trial,bufferWidth=0){
-  # nearestDiscord: nearest coordinate in the discordant arm, for the control
-  # coordinates return the minimal distance with a minus sign
-  dist_trial <- as.matrix(dist(trial[,c('x','y')], method = "euclidean"))
-  discord <- outer(trial$arm, trial$arm, "!=") #true & false.
-  discord_dist_trial <- ifelse(discord,dist_trial,Inf)
-  trial$nearestDiscord <- ifelse(trial$arm == 'control', -apply(discord_dist_trial, MARGIN=2, min), apply(discord_dist_trial, MARGIN=2, min))
+Specify_CRTbuffer <- function(trial = trial, bufferWidth = 0) {
+  # nearestDiscord: nearest coordinate in the discordant arm, for the control coordinates return the minimal
+  # distance with a minus sign
+  dist_trial <- as.matrix(dist(trial[, c("x", "y")], method = "euclidean"))
+  discord <- outer(trial$arm, trial$arm, "!=")  #true & false.
+  discord_dist_trial <- ifelse(discord, dist_trial, Inf)
+  trial$nearestDiscord <- ifelse(trial$arm == "control", -apply(discord_dist_trial, MARGIN = 2, min), apply(discord_dist_trial,
+                                                                                                            MARGIN = 2, min))
   # Find the row and column numbers of the nearest discordant pair of neighbours
-  discord_distance=ifelse(discord,0.2*round(abs(5*dist_trial),digits=1),Inf)
+  discord_distance <- ifelse(discord, 0.2 * round(abs(5 * dist_trial), digits = 1), Inf)
   # round off distances to nearest 20m to speed up calculation
-  discord_distance =
-    if(min(abs(discord_distance)) < bufferWidth){
-      trial$buffer = FALSE
-      pbuff=nbuff=width = 0
-      while (width < bufferWidth){
-        # round off distances to nearest 50m to speed up calculation
-        width = min(abs(discord_distance))
-        neighbours = which(abs(discord_distance) == min(abs(discord_distance)))
-        trial$buffer[neighbours %% nrow(trial)] = TRUE
+  discord_distance <- if (min(abs(discord_distance)) < bufferWidth) {
+    trial$buffer <- FALSE
+    pbuff <- nbuff <- width <- 0
+    while (width < bufferWidth) {
+      # round off distances to nearest 50m to speed up calculation
+      width <- min(abs(discord_distance))
+      neighbours <- which(abs(discord_distance) == min(abs(discord_distance)))
+      trial$buffer[neighbours%%nrow(trial)] <- TRUE
 
-        # Set distances for buffered locations to inf to exclude them from further iterations
-        discord_distance =
-          ifelse(abs(discord_distance) == width,Inf,discord_distance)
-        nbuff=sum(ifelse(trial$buffer,1,0))
-        cat('\rAdded',nbuff-pbuff,'locations to buffer (',width,'km)  ')
-        pbuff=nbuff
-      }
-      cat('\rLocations in buffer:',nbuff,'of',nrow(trial),'(',round(nbuff*100/nrow(trial),digits=1),'%)\n\n')
+      # Set distances for buffered locations to inf to exclude them from further iterations
+      discord_distance <- ifelse(abs(discord_distance) == width, Inf, discord_distance)
+      nbuff <- sum(ifelse(trial$buffer, 1, 0))
+      cat("\rAdded", nbuff - pbuff, "locations to buffer (", width, "km) ")
+      pbuff <- nbuff
     }
-  return(trial)}
+    cat("\rLocations in buffer:", nbuff, "of", nrow(trial), "(", round(nbuff * 100/nrow(trial), digits = 1), "%)\n\n")
+  }
+  return(trial)
+}
 
 #' Assign locations to clusters in a cluster randomized trial
 #'
 #' \code{DefineClusters} algorithmically assigns locations to clusters in a CRT with geographical clustering, by one of three algorithms:
-#' (i)    Nearest neighbour (NN): (assigns equal numbers of locations to each cluster)
-#' (ii)   kmeans clustering (kmeans) : (aims to partition locations so that each belongs to the cluster with the nearest centroid)
-#' (iii)  travelling salesman problem heuristic : assigns locations sequentially along a travelling salesman path
+#' (i) Nearest neighbour (NN): (assigns equal numbers of locations to each cluster)
+#' (ii) kmeans clustering (kmeans) : (aims to partition locations so that each belongs to the cluster with the nearest centroid)
+#' (iii) travelling salesman problem heuristic : assigns locations sequentially along a travelling salesman path
 #'
 #' @param trial dataframe containing (x,y) coordinates of households
-#' @param h  proposal for the number of locations in each cluster
-#' @param nclusters  number of clusters
-#' @param algo algorithm for cluster boundaries, options are: "TSP": travelling salesman problem heuristic; "NN": nearest neighbor; "kmeans": kmeans algorithm
+#' @param h proposal for the number of locations in each cluster
+#' @param nclusters number of clusters
+#' @param algo algorithm for cluster boundaries, options are: 'TSP': travelling salesman problem heuristic; 'NN': nearest neighbor; 'kmeans': kmeans algorithm
 #' @param reuseTSP indicator of whether a pre-existing path should be used by the TSP algorithm
 #' @return The input coordinates as a dataframe augmented with the following attributes
 #' \itemize{
@@ -133,45 +132,45 @@ Specify_CRTbuffer= function(trial=trial,bufferWidth=0){
 #' \item \code{cluster}: cluster assignments
 #' \item \code{h}: number of households in each cluster
 #' \item \code{algo}: algorithm used for cluster boundaries
-#' \item \code{path}: TSP path (if algo= 'TSP')
+#' \item \code{path}: TSP path (if algo = 'TSP')
 #' }
 #' @export
 #'
 #' @examples
-#' #Assign clusters to the test trial dataset averaging h=40 using the kmeans algorithm
-#' exampletrial = DefineClusters(trial=test_site, h=40, algo='kmeans')
+#' #Assign clusters to the test trial dataset averaging h = 40 using the kmeans algorithm
+#' exampletrial <- DefineClusters(trial = test_site, h = 40, algo = 'kmeans')
 
-DefineClusters = function(trial=trial, h=NULL, nclusters=NULL, algo='NN', reuseTSP=FALSE){
+DefineClusters <- function(trial = trial, h = NULL, nclusters = NULL, algo = "NN", reuseTSP = FALSE) {
 
-  TSP_ClusterDefinition <- function(coordinates,h,nclusters,reuseTSP){
+  TSP_ClusterDefinition <- function(coordinates, h, nclusters, reuseTSP) {
 
-    if(!"path" %in% colnames(coordinates) | !reuseTSP){
+    if (!"path" %in% colnames(coordinates) | !reuseTSP) {
       # Code originally from Silkey and Smith, SolarMal
 
       # Order the coordinates along an optimised travelling salesman path
       dist_coordinates <- dist(coordinates, method = "euclidean")
-      tsp_coordinates <- TSP::TSP(dist_coordinates) # object of class TSP
+      tsp_coordinates <- TSP::TSP(dist_coordinates)  # object of class TSP
       tsp_coordinates <- TSP::insert_dummy(tsp_coordinates)
-      tour <- TSP::solve_TSP(tsp_coordinates,"repetitive_nn") #solves TSP, expensive
-      path <- TSP::cut_tour(x=tour,cut='dummy')
+      tour <- TSP::solve_TSP(tsp_coordinates, "repetitive_nn")  #solves TSP, expensive
+      path <- TSP::cut_tour(x = tour, cut = "dummy")
       coordinates$path <- path
 
     }
-    #order coordinates
-    coordinates$order = seq(1:nrow(coordinates))
-    coordinates <- coordinates[order(coordinates$path),]
+    # order coordinates
+    coordinates$order <- seq(1:nrow(coordinates))
+    coordinates <- coordinates[order(coordinates$path), ]
 
-    n1 = (nclusters-1)*h
-    nclusters_1 = nclusters - 1
+    n1 <- (nclusters - 1) * h
+    nclusters_1 <- nclusters - 1
     # The last cluster may be a different size (if h is not a factor of the population size) )
-    coordinates$cluster = NA
-    coordinates$cluster[1:n1] <- c(rep(1:nclusters_1, each=h)) #add cluster assignment
-    coordinates$cluster[which(is.na(coordinates$cluster))] = nclusters
-    coordinates <- coordinates[order(coordinates$order),]
+    coordinates$cluster <- NA
+    coordinates$cluster[1:n1] <- c(rep(1:nclusters_1, each = h))  #add cluster assignment
+    coordinates$cluster[which(is.na(coordinates$cluster))] <- nclusters
+    coordinates <- coordinates[order(coordinates$order), ]
     return(coordinates)
   }
 
-  NN_ClusterDefinition <- function(coordinates,h,nclusters){
+  NN_ClusterDefinition <- function(coordinates, h, nclusters) {
 
     # algorithm is inspired by this website: ??? (comment from Lea)
 
@@ -179,61 +178,62 @@ DefineClusters = function(trial=trial, h=NULL, nclusters=NULL, algo='NN', reuseT
     dist_coordinates <- as.matrix(dist(coordinates, method = "euclidean"))
     coordinates$cluster <- NA
 
-    nclusters_1 = nclusters -1
-    for(i in 1:nclusters_1){
+    nclusters_1 <- nclusters - 1
+    for (i in 1:nclusters_1) {
 
-      #find unassigned coordinates
+      # find unassigned coordinates
       cluster_unassigned <- which(is.na(coordinates$cluster))
-      dist_coordinates_unassigned <- dist_coordinates[cluster_unassigned,cluster_unassigned]
+      dist_coordinates_unassigned <- dist_coordinates[cluster_unassigned, cluster_unassigned]
       cluster_na <- rep(NA, length(cluster_unassigned))
 
-      #find the coordinate furthest away from all the others
+      # find the coordinate furthest away from all the others
       index <- which.max(rowSums(dist_coordinates_unassigned))
 
-      #find the n nearest neighbors of index
-      cluster_na[head(order(dist_coordinates_unassigned[index,]), h)] <- i
-      coordinates$cluster[cluster_unassigned] = cluster_na
+      # find the n nearest neighbors of index
+      cluster_na[head(order(dist_coordinates_unassigned[index, ]), h)] <- i
+      coordinates$cluster[cluster_unassigned] <- cluster_na
     }
     # The last cluster may be a different size (if h is not a factor of the population size) )
-    coordinates$cluster[which(is.na(coordinates$cluster))] = nclusters
+    coordinates$cluster[which(is.na(coordinates$cluster))] <- nclusters
 
     return(coordinates)
   }
 
-  kmeans_ClusterDefinition <- function(coordinates,nclusters){
+  kmeans_ClusterDefinition <- function(coordinates, nclusters) {
 
-    #kmeans as implemented in R base
-    km <- kmeans(x=coordinates,centers=nclusters)
+    # kmeans as implemented in R base
+    km <- kmeans(x = coordinates, centers = nclusters)
     coordinates$cluster <- km$cluster
 
     return(coordinates)
   }
 
   # Local data from study area (ground survey and/or satellite images)
-  coordinates = data.frame(x=as.numeric(as.character(trial$x)),
-                           y=as.numeric(as.character(trial$y)))
+  coordinates <- data.frame(x = as.numeric(as.character(trial$x)), y = as.numeric(as.character(trial$y)))
 
-  # the number of clusters and the target cluster size must be integers.
-  # cluster size may vary slightly
-  if(is.null(nclusters)){ nclusters = ceiling(nrow(coordinates)/h)}
-  if(is.null(h)){ h = ceiling(nrow(coordinates)/nclusters)}
-  ##############################################################################
-  # derive cluster boundaries
-  ##############################################################################
+  # the number of clusters and the target cluster size must be integers.  cluster size may vary slightly
+  if (is.null(nclusters)) {
+    nclusters <- ceiling(nrow(coordinates)/h)
+  }
+  if (is.null(h)) {
+    h <- ceiling(nrow(coordinates)/nclusters)
+  }
+  ############################################################################## derive cluster boundaries
 
-  if(algo=='TSP'){
-    TSPoutput <- TSP_ClusterDefinition(coordinates,h,nclusters,reuseTSP)
-    trial$path = TSPoutput$path
-    trial$cluster = TSPoutput$cluster
-  } else if(algo == 'NN'){
-    trial$cluster <- NN_ClusterDefinition(coordinates,h,nclusters)$cluster
-  } else if(algo == 'kmeans'){
-    trial$cluster <- kmeans_ClusterDefinition(coordinates,nclusters)$cluster
+  if (algo == "TSP") {
+    TSPoutput <- TSP_ClusterDefinition(coordinates, h, nclusters, reuseTSP)
+    trial$path <- TSPoutput$path
+    trial$cluster <- TSPoutput$cluster
+  } else if (algo == "NN") {
+    trial$cluster <- NN_ClusterDefinition(coordinates, h, nclusters)$cluster
+  } else if (algo == "kmeans") {
+    trial$cluster <- kmeans_ClusterDefinition(coordinates, nclusters)$cluster
   } else {
-    stop('unknown method')
+    stop("unknown method")
   }
 
-  return(trial)}
+  return(trial)
+}
 
 
 #' Convert lat long co-ordinates to x,y
@@ -250,18 +250,18 @@ DefineClusters = function(trial=trial, h=NULL, nclusters=NULL, algo='NN', reuseT
 #' \item \code{y}: y co-ordinates
 #' }
 #' @export
-Convert_LatLong = function(df,latvar='lat',longvar='long'){
+Convert_LatLong <- function(df, latvar = "lat", longvar = "long") {
   colnames(df)[colnames(df) == latvar] <- "lat"
   colnames(df)[colnames(df) == longvar] <- "long"
-  R = 6371 # radius of the earth
-  latradians = with(df,pi/180*lat)
-  longradians = with(df,pi/180*long)
-  meanlat = mean(latradians)
-  meanlong = mean(longradians)
-  drops <- c("lat","long")
-  df=df[ , !(names(df) %in% drops)]
-  df$y = R * (latradians - meanlat)* cos(longradians)
-  df$x = R * (longradians - meanlong)
+  R <- 6371  # radius of the earth
+  latradians <- with(df, pi/180 * lat)
+  longradians <- with(df, pi/180 * long)
+  meanlat <- mean(latradians)
+  meanlong <- mean(longradians)
+  drops <- c("lat", "long")
+  df <- df[, !(names(df) %in% drops)]
+  df$y <- R * (latradians - meanlat) * cos(longradians)
+  df$x <- R * (longradians - meanlong)
   return(df)
 }
 
@@ -276,30 +276,29 @@ Convert_LatLong = function(df,latvar='lat',longvar='long'){
 #'
 #' @examples
 #' #Rotate and reflect test site locations
-#' transformedTestlocations=Anonymise_TrialSite()
+#' transformedTestlocations <- Anonymise_TrialSite()
 
-Anonymise_TrialSite = function(trial=CRTspillover::test_site){
-  # Local data from study area (ground survey and/or satellite images)
-  # random rotation angle
-  theta= 2 * pi * runif(n=1)
-  x= trial$x
-  y= trial$y
-  rangex =max(x) - min(x)
-  rangey =max(y) - min(y)
-  translation = c(rangex * rnorm(n=1), rangey*rnorm(n=1))
+Anonymise_TrialSite <- function(trial = CRTspillover::test_site) {
+  # Local data from study area (ground survey and/or satellite images) random rotation angle
+  theta <- 2 * pi * runif(n = 1)
+  x <- trial$x
+  y <- trial$y
+  rangex <- max(x) - min(x)
+  rangey <- max(y) - min(y)
+  translation <- c(rangex * rnorm(n = 1), rangey * rnorm(n = 1))
 
-  xy=t(matrix(c(x,y),ncol=2,nrow=length(x)))
-  xytranslated= xy+translation
+  xy <- t(matrix(c(x, y), ncol = 2, nrow = length(x)))
+  xytranslated <- xy + translation
 
-  rotation=matrix(c(cos(theta),sin(theta),-sin(theta),cos(theta)),nrow=2,ncol=2)
+  rotation <- matrix(c(cos(theta), sin(theta), -sin(theta), cos(theta)), nrow = 2, ncol = 2)
 
-  #Rotate
-  xytrans = rotation %*% xytranslated
+  # Rotate
+  xytrans <- rotation %*% xytranslated
 
   # Recentre on origin
-  recentred = xytrans - c(mean(xytrans[1,]),mean(xytrans[2,]))
-  trial$x = recentred[1,]
-  trial$y = recentred[2,]
+  recentred <- xytrans - c(mean(xytrans[1, ]), mean(xytrans[2, ]))
+  trial$x <- recentred[1, ]
+  trial$y <- recentred[2, ]
 
-  return(trial)}
-
+  return(trial)
+}
--- inst/extdata/examples.R
+++ inst/extdata/examples.R
@@ -1,54 +1,54 @@
-test1 = read.csv(file='data-raw/BagamoyoTrial.csv')
-test2=Convert_LatLong(test1)
-test3=Anonymise_TrialSite(trial=test2)
-test4=Randomize_CRT(test2)
-test5=Simulate_CRT(trial=test4,efficacy=0.4,initialPrevalence=0.5,sd=0.4)
-GEEresults=Analyse_CRT(trial=test5,method='GEE')
+test1 <- read.csv(file = 'C:/git_repos/trialdesign/RCode_data/BagamoyoTrial.csv')
+test2 <- Convert_LatLong(test1)
+test3 <- Anonymise_TrialSite(trial = test2)
+test4 <- Randomize_CRT(test2)
+test5 <- Simulate_CRT(trial = test4,efficacy = 0.4,initialPrevalence = 0.5,sd = 0.4)
+GEEresults <- Analyse_CRT(trial = test5,method = 'GEE')
 GEEresults$PointEstimates$efficacy
 GEEresults$IntervalEstimates$efficacy
 GEEresults$PointEstimates$ICC
-INLAresults=Analyse_CRT(trial=test5,method='LR')
+INLAresults <- Analyse_CRT(trial = test5,method = 'LR')
 INLAresults$PointEstimates$efficacy
 INLAresults$IntervalEstimates$efficacy
 INLAresults$PointEstimates$contamination$contaminationRange
 
-test6=Specify_CRTbuffer(trial = test5, bufferWidth = 0.5)
-trial=test_Simulate_CRT
-method='GEE'
-requireBootstrap=FALSE
-confidence_level=0.2
-excludeBuffer=FALSE
+test6 <-Specify_CRTbuffer(trial = test5, bufferWidth = 0.5)
+trial <-test_Simulate_CRT
+method <- 'GEE'
+requireBootstrap <- FALSE
+confidence_level <- 0.2
+excludeBuffer <- FALSE
 
-test1 = read.csv(file='data-raw/Solarmal_baseline.csv')
-test2=Convert_LatLong(df=test1)
-test2$num=test2$RDT_test_result
-test2=with(test2,test2[,c('x','y','num')])
-test3=Aggregate_CRT(test2)
-Plot_CRTmap(test3,maskbuffer=0.5)
-test4 = DefineClusters(trial=test3, h=50, algo='NN')
-Plot_CRTmap(test4,maskbuffer=0.5)
-test5 = Randomize_CRT(test4)
-Plot_CRTmap(test5,maskbuffer=0.5)
+test1 <- read.csv(file = 'data-raw/Solarmal_baseline.csv')
+test2 <- Convert_LatLong(df = test1)
+test2$num <- test2$RDT_test_result
+test2 <- with(test2,test2[,c('x','y','num')])
+test3 <- Aggregate_CRT(test2)
+Plot_CRTmap(test3,maskbuffer = 0.5)
+test4 <- DefineClusters(trial = test3, h = 50, algo = 'NN')
+Plot_CRTmap(test4,maskbuffer = 0.5)
+test5 <- Randomize_CRT(test4)
+Plot_CRTmap(test5,maskbuffer = 0.5)
 
-ICC=c()
-clusterSize = seq(1:10)*20
+ICC <- c()
+clusterSize <- seq(1:10)*20
 for(size in clusterSize){
-  test4a=DefineClusters(trial=test3, h=size, algo='NN')
-  test4b= Randomize_CRT(test4a)
-  baselineAnalysis = calculateICC_baseline(trial=test4b)
-  ICC=c(ICC,baselineAnalysis$estimates$ICC)
+ test4a <- DefineClusters(trial = test3, h = size, algo = 'NN')
+ test4b <- Randomize_CRT(test4a)
+ baselineAnalysis <- calculateICC_baseline(trial = test4b)
+ ICC <- c(ICC,baselineAnalysis$estimates$ICC)
 }
 plot(clusterSize,ICC)
 
-estimates=methods=c()
-all_methods = c('piecewise_linear','logit','sigmoid','empirical',
-                'GEE','MCMC01','MCMC02','MCMC03')
+estimates <- methods <- c()
+all_methods <- c('piecewise_linear','logit','sigmoid','empirical',
+ 'GEE','MCMC01','MCMC02','MCMC03')
 for(method in all_methods){
-  print(method)
-  res = Analyse_CRT(trial=test_Simulate_CRT,method=method)
-  estimates = c(estimates,res$PointEstimates$efficacy)
+ print(method)
+ res <- Analyse_CRT(trial = test_Simulate_CRT,method = method)
+ estimates = c(estimates,res$PointEstimates$efficacy)
 }
-names(estimates)=all_methods
+names(estimates) <- all_methods
 estimates
 
 
